1c884bc638e8b57a5cab01fd04d1de8bc75bd3ba
kevinnunes <kevin.nunes@he-arc.ch>
2016-11-04 11:06:10 +0100

    Scrool Smooth JQuery application

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,66 @@
+$(document).ready(function() {
+    $('ul li').eq(1).click(function() {
 
+        'use strict';
+        $.smoothScroll({
+            scrollTarget: '#Objectif'
+        });
+        return false;
+    });
+    $('ul li').eq(2).click(function() {
+
+        'use strict';
+        $.smoothScroll({
+            scrollTarget: '#Formations'
+        });
+        return false;
+    });
+    $('ul li').eq(3).click(function() {
+
+        'use strict';
+        $.smoothScroll({
+            scrollTarget: '#Experiences'
+        });
+        return false;
+    });
+    $('ul li').eq(4).click(function() {
+
+        'use strict';
+        $.smoothScroll({
+            scrollTarget: '#Langues'
+        });
+        return false;
+    });
+    $('ul li').eq(5).click(function() {
+
+        'use strict';
+        $.smoothScroll({
+            scrollTarget: '#Informatique'
+        });
+        return false;
+    });
+    $('ul li').eq(6).click(function() {
+
+        'use strict';
+        $.smoothScroll({
+            scrollTarget: '#Loisirs'
+        });
+        return false;
+    });
+    $('ul li').eq(7).click(function() {
+
+        'use strict';
+        $.smoothScroll({
+            scrollTarget: '#References'
+        });
+        return false;
+    });
+    $('a[href*="#hautdepage"]').click(function() {
+
+        'use strict';
+        $.smoothScroll({
+            scrollTarget: '#hautdepage'
+        });
+        return false;
+    });
+});
