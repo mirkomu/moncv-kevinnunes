4503757fe5d294a72e70c4de0244a67b9da68ef6
kevinnunes <kevin.nunes@he-arc.ch>
2016-11-07 07:46:11 +0100

    Charts.js installation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -63,4 +63,22 @@ $(document).ready(function() {
         });
         return false;
     });
+    var bar = new ProgressBar.Circle(container, {
+        color: '#FFEA82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        duration: 1400,
+        easing: 'bounce',
+        strokeWidth: 6,
+        from: { color: '#FFEA82', a: 0 },
+        to: { color: '#ED6A5A', a: 1 },
+        // Set default step function for all animate calls
+        step: function(state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+        }
+    });
+
+    $(".progress").ready(function() {
+        bar.animate(1.0); // Number from 0.0 t
+    });
 });
